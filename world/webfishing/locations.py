0441217d6a33cfdc24c893a97aae0489c71b8c75
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2025-04-21 12:44:25 +0300

    Add location name groups
    Add the following groups:
    Initial Quests (every unique quest eg. Catch Lake Fish 1/5)
    Extra Quests (repeated quests eg. Catch Lake Fish 2/5)
    Cheap Shop Items
    Moderate Shop Items
    Expensive Shop Items
    Camp Shop Items
    Spectral Bones

--- world/webfishing/locations.py
+++ world/webfishing/locations.py
@@ -47,24 +47,41 @@ def populate_advancement_tables():
     # gcq_count lots of generic catch quests
     for i, c in enumerate(gcq_base):
         for x in range(gcq_count):
-            base[f"{c} Quest ({x + 1}/{gcq_count})"] = AdvData(quest_offset + counter)
+            check = f"{c} Quest ({x + 1}/{gcq_count})"
+            if x == 0:
+                quests_1_group.append(check)
+            else:
+                quests_2_group.append(check)
+            base[check] = AdvData(quest_offset + counter)
             counter += 1
 
     # gcq_count lots of generic catch quests
     for i, c in enumerate(gcq_hard):
         for x in range(gcq_count):
-            hard[f"{c} Quest ({x + 1}/{gcq_count})"] = AdvData(quest_offset + counter)
+            check = f"{c} Quest ({x + 1}/{gcq_count})"
+            if x == 0:
+                quests_1_group.append(check)
+            else:
+                quests_2_group.append(check)
+            hard[check] = AdvData(quest_offset + counter)
             counter += 1
 
     # 2 lots of (all) normal specific catch quests (>1% chance according to wiki, no extra bait req.)
     for i, c in enumerate(scq_normal):
         for x in range(2):
-            base[f"{c} Quest ({x + 1}/2)"] = AdvData(quest_offset + counter)
+            check = f"{c} Quest ({x + 1}/2)"
+            if x == 0:
+                quests_1_group.append(check)
+            else:
+                quests_2_group.append(check)
+            base[check] = AdvData(quest_offset + counter)
             counter += 1
 
     # 1 of hard specific catch quests (so only need to catch 1, no second check for 5)
     for c in scq_hard:
-        hard_specific[f"{c} Quest (1/1)"] = AdvData(quest_offset + counter)
+        check = f"{c} Quest (1/1)"
+        quests_1_group.append(check)
+        hard_specific[check] = AdvData(quest_offset + counter)
         counter += 1
 
     # --- MISC --- #
@@ -76,6 +93,7 @@ def populate_advancement_tables():
     for c in base_misc:
         base[c] = AdvData(item_offset + counter)
         counter += 1
+    spectral_group.extend(base_misc)
 
     # --- SHOPS --- #
 
@@ -85,18 +103,21 @@ def populate_advancement_tables():
     # cheap shop checks
     for i in range(16):
         check = f"Cheap Shop Purchase ({i+1})"
+        shop_1_group.append(check)
         base[check] = AdvData(shop_offset + counter)
         counter += 1
 
     # costly shop checks
     for i in range(8):
         check = f"Moderate Shop Purchase ({i+1})"
+        shop_2_group.append(check)
         medium[check] = AdvData(shop_offset + counter)
         counter += 1
 
     # expensive shop checks
     for i in range(8):
         check = f"Expensive Shop Purchase ({i+1})"
+        shop_3_group.append(check)
         hard[check] = AdvData(shop_offset + counter)
         counter += 1
 
@@ -112,6 +133,7 @@ def populate_advancement_tables():
 
     camp_4["Progression Shop Purchase (4)"] = AdvData(shop_offset + counter)
     counter += 1
+    camp_shop_group.extend(["Progression Shop Purchase (1)", "Progression Shop Purchase (2)", "Progression Shop Purchase (3)", "Progression Shop Purchase (4)"])
 
 
 base = {}
@@ -121,6 +143,15 @@ hard_specific = {}
 camp_2 = {}
 camp_3 = {}
 camp_4 = {}
+
+# Location groups
+quests_1_group = []
+quests_2_group = []
+shop_1_group = []
+shop_2_group = []
+shop_3_group = []
+camp_shop_group = []
+spectral_group = []
 
 quest_offset: int = 94200
 item_offset: int = 94700
